0e5d50dc2bf1717c1e65946442e4865693037bc4
olha1313 <oliapankiv13@gmail.com>
2019-08-30 22:28:17 +0300

    Add slider

---
+++ app/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    autoplay: true,
+    dots: true,
+    arrows: true,
+    zIndex: 1
+});
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,35 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const autoprefixer = require('autoprefixer')
+const browserSync = require('browser-sync')
+const sourcemaps = require('gulp-sourcemaps')
+const postcss = require('gulp-postcss');
 
-gulp.task('watch', () => {
-  gulp.watch('app/scss/**/*', function watch() {
-    return gulp.src('app/scss/**/*')
-    .pipe(sass({ outputStyle: 'expanded'})
-      .on('error', sass.logError))
-    .pipe(gulp.dest('app/css'))
-  })
+gulp.task('sass', () => {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass()).on('error', sass.logError)
+        .pipe(postcss([autoprefixer(['last 3 versions'])]))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('watch',  () => {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    })
+    gulp.watch('app/scss/**/*.scss', gulp.series('sass'))
+});
+
+gulp.task('js', () => {
+    return gulp.src('app/js/*.js')
+        .pipe(browserSync.stream())
 })
+
+gulp.task('default', gulp.parallel('watch', () => {
+    gulp.watch('app/*.html').on('change', browserSync.reload);
+    gulp.watch("app/js/*.js", gulp.series('js'))
+}));
