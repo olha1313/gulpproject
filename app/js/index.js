a146897c1417cd28a82f276018120511672a340d
olha1313 <oliapankiv13@gmail.com>
2019-09-13 21:26:55 +0300

    Improve header

--- app/js/index.js
+++ app/js/index.js
@@ -10,11 +10,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("header").style.padding = "10px 0";
+    document.getElementById("header").style.padding = "10px 10px";
     document.getElementById("blue-logo").style.height = "25px";
     document.getElementById("blue-logo").style.width = "125px";
   } else {
-    document.getElementById("header").style.padding = "30px 0";
+    document.getElementById("header").style.padding = "30px 10px";
     document.getElementById("blue-logo").style.height = "40px";
     document.getElementById("blue-logo").style.width = "162px";
   }
