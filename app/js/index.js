ea51a6305f860533c429160d34ecf94d98d85804
olha1313 <oliapankiv13@gmail.com>
2019-09-17 13:14:00 +0300

    Change some layout

--- app/js/index.js
+++ app/js/index.js
@@ -6,15 +6,15 @@ $(document).ready(function(){
 });
 
 
-window.onscroll = function() {scrollFunction()};
+window.scroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("header").style.padding = "10px 10px";
+  if (window.scrollTop > 80) {
+    document.getElementById("header").addClass('fixed');
     document.getElementById("blue-logo").style.height = "25px";
     document.getElementById("blue-logo").style.width = "125px";
   } else {
-    document.getElementById("header").style.padding = "30px 10px";
+    document.getElementById("header").removeClass('fixed');
     document.getElementById("blue-logo").style.height = "40px";
     document.getElementById("blue-logo").style.width = "162px";
   }
