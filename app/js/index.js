b3e08450ac0e13d6b21e7e44fccb47426ef08936
olha1313 <oliapankiv13@gmail.com>
2019-10-11 16:22:56 +0300

    Some change

--- app/js/index.js
+++ app/js/index.js
@@ -1,21 +1,7 @@
 $(document).ready(function(){
   $('.slider').slick({
-    dots: true
-    // autoplay: true
+    dots: true,
+    autoplay: true
   });
 });
 
-
-window.scroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (window.scrollTop > 80) {
-    document.getElementById("header").addClass('fixed');
-    document.getElementById("blue-logo").style.height = "25px";
-    document.getElementById("blue-logo").style.width = "125px";
-  } else {
-    document.getElementById("header").removeClass('fixed');
-    document.getElementById("blue-logo").style.height = "40px";
-    document.getElementById("blue-logo").style.width = "162px";
-  }
-}
